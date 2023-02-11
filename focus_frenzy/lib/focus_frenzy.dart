52e6e08d54fd9dd1179609d1ef77f9ad293fa9d5
philliplumod <philliplumod@gmail.com>
2023-02-11 21:10:31 +0800

    focus frenzy

---
+++ focus_frenzy/lib/focus_frenzy.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FocusFrenzy extends StatelessWidget {
+  const FocusFrenzy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Focus Frenzy')),
+    );
+  }
+}
---
+++ focus_frenzy/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:focus_frenzy/focus_frenzy.dart';
+
+void main() => runApp(const MaterialApp(
+      home: FocusFrenzy(),
+    ));
